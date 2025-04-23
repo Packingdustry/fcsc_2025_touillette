945a6fe448c8e748c124dc1b24019c04e19ce273
Packingdustry <alois.mc@hotmail.com>
2025-04-23 23:23:44 +0200

    résultat final

--- resoudre.py
+++ resoudre.py
@@ -1,11 +1,22 @@
 encrypted = open("output.txt").read()
 
-res = ['.'] * 64
+res2 = ['.'] * 64
 
+# Replacement des nombres décalés de 2 à la fin
 for i in range(0, 32):
-    res[i*2+1] = encrypted[i]
+    res2[i*2+1] = encrypted[i]
 
 for i in range(32, 64):
-    res[(i-32)*2] = encrypted[i]
+    res2[(i-32)*2] = encrypted[i]
 
-print("".join(res))
+# Replacement des nombres décalés de 8
+res8 = ['']*8
+print(res8)
+for i in range(0, 64):
+    for j in range(0, 8):
+        print("j :", j, "i : ", i)
+        if i%8==j:
+            res8[j] += res2[i]
+
+res8.reverse()
+print(''.join(res8))
